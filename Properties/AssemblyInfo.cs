060aa226b6b32439ae11b941ae7d388c8c0825b3
Allis Tauri <allista@gmail.com>
2020-05-03 18:40:03 +0300

    AssemblyVersion

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using AT_Utils;
 #if NIGHTBUILD
 [assembly: AssemblyVersion("0.1.*")]
 #else
-[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyVersion("0.1.0.2")]
 #endif
 [assembly: KSPAssembly("CargoAccelerators", 0, 1)]
 
